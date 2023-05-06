b309b6097fa69cb9a850bc6b7c40a75b8253d5f6
Sergey Grabak <seventyseventh@gmail.com>
2023-05-06 19:42:38 +0200

    fix: eslint errors resolve

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,6 +3,10 @@ const path = require('path');
 
 const reader = fs.createReadStream(path.resolve(__dirname, 'text.txt'));
 
+reader.on('error', function (error) {
+  console.log('Error reading file: ', error.message);
+});
+
 reader.on('data', function (chunk) {
-    console.log(chunk.toString());
+  console.log(chunk.toString());
 });
--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,31 +2,28 @@ const fs = require('fs');
 const path = require('path');
 const readline = require('node:readline');
 
-const ac = new AbortController();
-const signal = ac.signal;
-
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
-const writer = fs.createWriteStream(path.resolve(__dirname, 'text.txt'), {flags:'a'});
+const writer = fs.createWriteStream(path.resolve(__dirname, 'text.txt'), { flags: 'a' });
 
 writer.on('ready', () => {
-    console.log(`Hello! Write something\n`);
+  console.log('Hello! Write something\n');
 
-    rl.on('line', (line) => {
-        if (line === 'exit') {
-            writer.close();
+  rl.on('line', (line) => {
+    if (line === 'exit') {
+      writer.close();
 
-            rl.close();
+      rl.close();
 
-            return;
-        }
-        writer.write(`${line}\n`);
-    });
+      return;
+    }
+    writer.write(`${line}\n`);
+  });
 
-    rl.on('close', () => {
-        console.log('Thank you! Bye');
-    })
+  rl.on('close', () => {
+    console.log('Thank you! Bye');
+  });
 });
