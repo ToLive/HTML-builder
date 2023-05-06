75efa183afaf3fee8714255760eac59af37e1704
Sergey Grabak <seventyseventh@gmail.com>
2023-05-06 22:41:59 +0200

    feat: task 04-copy-directory ready

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const fsPromises = require('node:fs/promises');
+const path = require('path');
 
+const app = async () => {
+  try {
+    const baseFolder = path.resolve(__dirname,'files');
+    const projectFolder = path.resolve(__dirname,'files-copy');
+    
+    await fsPromises.mkdir(projectFolder, { recursive: true });
+    
+    const files = await fsPromises.readdir(baseFolder);
+
+    for (const file of files) {
+      const oldDestination = path.join(baseFolder, file);      
+      const newDestination = path.join(projectFolder, file);
+
+      await fsPromises.copyFile(oldDestination, newDestination);
+    }
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+app();
